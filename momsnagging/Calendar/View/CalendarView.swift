005d1b163e44e9cd629f6c91146f45412fcb4f64
connor.jeon@babyfreinds.us <connor.jeon@babyfriends.us>
2022-03-26 19:54:30 +0900

    [ Feat ] _ 달력 디자인 제외 구성 완료, 디자인 나오면 맞춰서 레이아웃 수정 진행 예정

---
+++ momsnagging/Calendar/View/CalendarView.swift
@@ -0,0 +1,122 @@
+//
+//  CalendarView.swift
+//  momsnagging
+//
+//  Created by 전창평 on 2022/03/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+
+class CalendarView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initUI()
+        layoutSetting()
+        bind()
+    }
+    //MARK: - Properties & Variable
+    var viewModel = CalenderViewModel()
+    var weekDay:Array<String>?
+    var daylist:Array<String>?
+    //MARK: - UI Properties
+    lazy var weekDayCollectionView : UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: weekDayCellLayout())
+        collectionView.register(CalendarWeekDayCell.self, forCellWithReuseIdentifier: "CalendarWeekDayCell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.tag = 0
+        return collectionView
+    }()
+    func weekDayCellLayout() -> UICollectionViewFlowLayout{
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0,left: 0,bottom: 0,right: 0)
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 7, height: 50)
+        return layout
+    }
+    lazy var dayCollectionView : UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: dayDayCellLayout())
+        collectionView.register(CalendarDayCell.self, forCellWithReuseIdentifier: "CalendarDayCell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.tag = 1
+        return collectionView
+    }()
+    func dayDayCellLayout() -> UICollectionViewFlowLayout{
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0,left: 0,bottom: 0,right: 0)
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 7, height: 80)
+        return layout
+    }
+    //MARK: - InitUI
+    func initUI(){
+        weekDay = viewModel.weekDayList()
+        daylist = viewModel.dayList()
+    }
+    //MARK: - Layout Setting
+    func layoutSetting(){
+        view.addSubview(weekDayCollectionView)
+        view.addSubview(dayCollectionView)
+        weekDayCollectionView.snp.makeConstraints({
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.equalTo(view.snp.leading)
+            $0.trailing.equalTo(view.snp.trailing)
+            $0.height.equalTo(50)
+        })
+        dayCollectionView.snp.makeConstraints({
+            $0.top.equalTo(weekDayCollectionView.snp.bottom)
+            $0.leading.equalTo(view.snp.leading)
+            $0.trailing.equalTo(view.snp.trailing)
+            $0.height.equalTo(50 * viewModel.rowCount())
+        })
+    }
+    //MARK: - Bind
+    func bind(){
+        
+    }
+    //MARK: - Other
+}
+//MARK: - Action
+extension CalendarView {
+}
+
+//MARK: - UICollectionView Delegate, Datasource
+extension CalendarView : UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView.tag == 0 {
+            return weekDay?.count ?? 0
+        }else{
+            return daylist?.count ?? 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView.tag == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CalendarWeekDayCell", for: indexPath) as! CalendarWeekDayCell
+            let row = weekDay?[indexPath.row]
+            cell.dayWeekLabel.text = row
+            return cell
+        }else{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CalendarDayCell", for: indexPath) as! CalendarDayCell
+            let row = daylist?[indexPath.row]
+            if row == "" {
+                cell.emoji.isHidden = true
+            }else{
+                cell.number.text = row
+                cell.emoji.isHidden = false
+            }
+            return cell
+        }
+    }
+}
---
+++ momsnagging/Calendar/View/CalendarWeekDayCell.swift
@@ -0,0 +1,29 @@
+//
+//  CalendarWeekDayCell.swift
+//  momsnagging
+//
+//  Created by 전창평 on 2022/03/26.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class CalendarWeekDayCell: UICollectionViewCell {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setUI()
+    }
+    lazy var dayWeekLabel = UILabel().then({
+        $0.textColor = UIColor.white
+        $0.font = .systemFont(ofSize: 17, weight: .bold)
+    })
+}
+extension CalendarWeekDayCell {
+    private func setUI(){
+        contentView.addSubview(dayWeekLabel)
+        dayWeekLabel.snp.makeConstraints({
+            $0.center.equalTo(contentView.snp.center)
+        })
+    }
+}
