b87fa6b375c68b692c60d78636b5c72ccdf2134e
jcp1452@gmail.com <connor.jeon@babyfriends.us>
2022-04-11 01:40:51 +0900

    [ Feat ] _ 이전달, 다음달 데이터 세팅 완료, 컬렉션뷰 처리를 Observable [View] -> [ViewModel]로 이동 바인딩 처리 끝, 기타 모델 추가 _ 함수명이 이상할시 Refactor 예정.

--- momsnagging/Calendar/Model/CalendarModel.swift
+++ momsnagging/Calendar/Model/CalendarModel.swift
@@ -10,8 +10,11 @@ import Foundation
 class CalendarModel {
     var weekDayList: [String] = ["월", "화", "수", "목", "금", "토", "일"]
     /// 윤년 확인(2월 일수 확인을 위함)
-    func checkLeapMonth() -> Bool {
-        let year = getYear()
+    func checkLeapMonth(currentYear: Int?=nil) -> Bool {
+        var year = getYear()
+        if let currentYear = currentYear {
+            year = currentYear
+        }
         if year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) {
             return true
         } else {
@@ -33,15 +36,50 @@ class CalendarModel {
         let month: Int = Date().getMonth()
         return month
     }
+    // 저번달 가져오기
+    func getLastMonth(currentMonth: Int) -> Int {
+        var month: Int = 0
+        if currentMonth == 1 {
+            month = 12
+        } else {
+            month = currentMonth - 1
+        }
+        return month
+    }
+    /// 저번연도 가져오기
+    func getLastYear(currentYear: Int) -> Int {
+        var year: Int = 0
+        year = currentYear - 1
+        return year
+    }
+    /// 다음달 가져오기
+    func getNextMonth(currentMonth: Int) -> Int {
+        var month: Int = 0
+        if currentMonth == 12 {
+            month = 1
+        } else {
+            month = currentMonth + 1
+        }
+        return month
+    }
+    /// 내년도 가져오기
+    func getNextYear(currentYear: Int) -> Int {
+        var year: Int = 0
+        year = currentYear + 1
+        return year
+    }
     /// 이번주 몇번째 요일인지 일(0)~토(6)
     func getWeekDay() -> Int {
         let weekDay: Int = Date().getWeekDay()
         return weekDay
     }
     /// 이번달 일수
-    func getMonthDaysCount() -> Int {
+    func getMonthDaysCount(currentMonth: Int?=nil, currentYear: Int?=nil) -> Int {
         var count = 0
-        let month = Date().getMonth()
+        var month = Date().getMonth()
+        if let currentMonth = currentMonth {
+            month = currentMonth
+        }
         let month31 = [1, 3, 5, 7, 8, 10, 12]
         let month30 = [4, 6, 9, 11]
         let month29 = [2]
@@ -53,10 +91,12 @@ class CalendarModel {
             count = 30
         }
         for i in month29 where i == month {
-            if checkLeapMonth() {
-                count = 29
-            } else {
-                count = 28
+            if let currentYear = currentYear {
+                if checkLeapMonth(currentYear: currentYear) {
+                    count = 29
+                } else {
+                    count = 28
+                }
             }
         }
         return count
@@ -72,14 +112,49 @@ class CalendarModel {
                 count = 6
             }
         }
+        print("이번달 시작 요일 : \(count)")
         return count
     }
+    /// 입력 날짜의 요일 가져오기 월:0 ~ 일:6
+    func getDayOfWeek(_ today: String) -> Int? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        guard let todayDate = formatter.date(from: today) else { return nil }
+        let myCalendar = Calendar(identifier: .gregorian)
+        let weekDay = myCalendar.component(.weekday, from: todayDate)
+        var i = 0
+        switch weekDay {
+        case 1:
+            i = 6
+        case 2:
+            i = 0
+        case 3:
+            i = 1
+        case 4:
+            i = 2
+        case 5:
+            i = 3
+        case 6:
+            i = 4
+        case 7:
+            i = 5
+        default:
+            break
+        }
+        return i
+    }
     /// 이번달 행수 = 콜렉션뷰높이 설정용
-    func getMonthRow() -> Int {
+    func getMonthRow(currentMonth: Int, currentYear: Int) -> Int {
         var count = 0
-        var monthDayCount = getMonthDaysCount()
-        let startWeekDay = getMonthFirstDayWeekDay()
-        monthDayCount -= (7 - startWeekDay)
+        var monthDayCount = getMonthDaysCount(currentMonth: currentMonth, currentYear: currentYear)
+        var startWeekDayString: String = ""
+        if currentMonth > 9 {
+            startWeekDayString = "\(currentYear)-\(currentMonth)-01"
+        } else {
+            startWeekDayString = "\(currentYear)-\(currentMonth)-01"
+        }
+        let startWeekDay = getDayOfWeek(startWeekDayString)
+        monthDayCount -= (7 - startWeekDay!)
         count += 1
         count += (monthDayCount / 7)
         monthDayCount -= (7 * (monthDayCount / 7))
@@ -88,22 +163,43 @@ class CalendarModel {
         }
         return count
     }
-    /// 이번달 일 list (시작요일에 맞추기 위해 빈 cell 추가) + (이번달 일수 추가)
-    func getDayList() -> [String] {
+    /// 이번달 일 list (시작요일에 맞추기 위해 빈 cell 추가) + (이번달 일수 추가)  최초 세팅시에만 호출
+    func getDefaultDayList() -> [String] {
         let startWeekDay = getMonthFirstDayWeekDay()
         var dayList = [String]()
         for _ in 0..<(startWeekDay) {
-            dayList.append("")
+            dayList.append("emptyCell")
         }
         for i in 1...getMonthDaysCount() {
             dayList.append("\(i)")
         }
         return dayList
     }
-    /// 저번달 일수
-    func getLastMonthDaysCount() -> Int {
+    /// 이전달, 다음달로 넘어갈때 일 list 값 전달
+    func getOtherMonthDayList(currentMonth: Int, currentYear: Int) -> [String] {
+        var startWeekDayString: String = ""
+        if currentMonth > 9 {
+            startWeekDayString = "\(currentYear)-\(currentMonth)-01"
+        } else {
+            startWeekDayString = "\(currentYear)-0\(currentMonth)-01"
+        }
+        let startWeekDay = getDayOfWeek(startWeekDayString)
+        var dayList = [String]()
+        for _ in 0..<(startWeekDay!) {
+            dayList.append("emptyCell")
+        }
+        for i in 1...getMonthDaysCount(currentMonth: currentMonth, currentYear: currentYear) {
+            dayList.append("\(i)")
+        }
+        return dayList
+    }
+    /// 저번달 일수 _ 주간 달력 데이터에서 사용
+    func getLastMonthDaysCount(currentMonth: Int?=nil, currentYear: Int) -> Int {
         var count = 0
-        var month = Date().getMonth() - 1
+        var month = getMonth()
+        if let currentMonth = currentMonth {
+            month = currentMonth - 1
+        }
         if month == 0 {
             month = 12
         }
@@ -113,12 +209,11 @@ class CalendarModel {
         for i in month31 where i == month {
             count = 31
         }
-        
         for i in month30 where i == month {
             count = 30
         }
         for i in month29 where i == month {
-            if checkLeapMonth() {
+            if checkLeapMonth(currentYear: currentYear) {
                 count = 29
             } else {
                 count = 28
@@ -126,11 +221,11 @@ class CalendarModel {
         }
         return count
     }
-    /// 주간 달력 데이터
-    func getWeek() -> [Int] {
+    // 주간 달력 데이터
+    func getWeek(currentMonth: Int, currentYear: Int) -> [Int] {
         let today = getToday()
         let weekDay = getWeekDay()
-        var lastMonthDay = getLastMonthDaysCount()
+        var lastMonthDay = getLastMonthDaysCount(currentMonth: currentMonth, currentYear: currentYear)
         var lastMonthDayCount = 0
         var nextMonthDay = 1
         var weekDayList = [Int]()
@@ -145,7 +240,7 @@ class CalendarModel {
         for i in weekDayList where i <= 0 {
             lastMonthDayCount += 1
         }
-        for i in 0..<weekDayList.count where weekDayList[i] > getMonthDaysCount() {
+        for i in 0..<weekDayList.count where weekDayList[i] > getMonthDaysCount(currentMonth: currentMonth, currentYear: currentYear) {
             weekDayList[i] = nextMonthDay
             nextMonthDay += nextMonthDay
         }
--- momsnagging/Calendar/ViewModel/CalendarViewModel.swift
+++ momsnagging/Calendar/ViewModel/CalendarViewModel.swift
@@ -11,19 +11,45 @@ import Moya
 
 class CalendarViewModel {
     
+    // MARK: - Observable
+    var monthObservable = BehaviorSubject<Int>(value: CalendarModel().getMonth()) // 최초 현재월 방출
+    var yearObservable = BehaviorSubject<Int>(value: CalendarModel().getYear()) // 최초 현재 연도 방출
+    
+    var weekDay = BehaviorSubject<[String]>(value: CalendarModel().weekDayList) // 월~일 리스트 방출
+    var daylist = BehaviorSubject<[String]>(value: CalendarModel().getDefaultDayList()) // 최초 이번달 일수 리스트 방출
+    var weekDayListObservable = PublishSubject<[Int]>() // 이번주 주간 리스트 옵저버블
+    
+    var collectionViewHeight = PublishSubject<Int>() // 해당 월의 행수(row)
     // MARK: - Properties & Variable
     private var model = CalendarModel()
-    
-    func dayList() -> [String] {
-        return model.getDayList()
+    /// 지난달 가져오기
+    func getLastMonth(currentMonth: Int, currentYear: Int) {
+        if model.getLastMonth(currentMonth: currentMonth) == 12 {
+            yearObservable.onNext(model.getLastYear(currentYear: currentYear))
+            daylist.onNext(model.getOtherMonthDayList(currentMonth: model.getLastMonth(currentMonth: currentMonth), currentYear: model.getLastYear(currentYear: currentYear)))
+            collectionViewHeight.onNext(rowCount(currentMonth: model.getLastMonth(currentMonth: currentMonth), currentYear: model.getLastYear(currentYear: currentYear)))
+        }
+        monthObservable.onNext(model.getLastMonth(currentMonth: currentMonth))
+        daylist.onNext(model.getOtherMonthDayList(currentMonth: model.getLastMonth(currentMonth: currentMonth), currentYear: currentYear))
+        collectionViewHeight.onNext(rowCount(currentMonth: model.getLastMonth(currentMonth: currentMonth), currentYear: currentYear))
     }
-    func rowCount() -> Int {
-        return model.getMonthRow()
+    /// 다음달 데이터 가져오기
+    func getNextMonth(currentMonth: Int, currentYear: Int) {
+        if model.getNextMonth(currentMonth: currentMonth) == 1 {
+            yearObservable.onNext(model.getNextYear(currentYear: currentYear))
+            daylist.onNext(model.getOtherMonthDayList(currentMonth: model.getNextMonth(currentMonth: currentMonth), currentYear: model.getNextYear(currentYear: currentYear)))
+            collectionViewHeight.onNext(rowCount(currentMonth: model.getLastMonth(currentMonth: currentMonth), currentYear: model.getLastYear(currentYear: currentYear)))
+        }
+        monthObservable.onNext(model.getNextMonth(currentMonth: currentMonth))
+        daylist.onNext(model.getOtherMonthDayList(currentMonth: model.getNextMonth(currentMonth: currentMonth), currentYear: currentYear))
+        collectionViewHeight.onNext(rowCount(currentMonth: model.getLastMonth(currentMonth: currentMonth), currentYear: currentYear))
     }
-    func weekDayList() -> [String] {
-        return model.weekDayList
+    /// 주수 카운트 (이번 달의 행(row)수)
+    func rowCount(currentMonth: Int, currentYear: Int) -> Int {
+        return model.getMonthRow(currentMonth: currentMonth, currentYear: currentYear)
     }
-    func getWeekCalendarData() -> [Int] {
-        return model.getWeek()
+    /// 주간 리스트
+    func weekDayList(currentMonth: Int, currentYear: Int) {
+        weekDayListObservable.onNext(model.getWeek(currentMonth: currentMonth, currentYear: currentYear))
     }
 }
