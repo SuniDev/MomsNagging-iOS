d89cf67b6c3bd3c9fb53e54da16b13eeb87bae06
jcp1452@gmail.com <connor.jeon@babyfriends.us>
2022-05-01 22:04:45 +0900

    [ Feat ] _ Home 월간달력 생성. variable, 로직 등 필요없는 데이터 확인 후 Refactor 필요합니다.

--- momsnagging/Modules/Home/View/HomeView.swift
+++ momsnagging/Modules/Home/View/HomeView.swift
@@ -14,6 +14,7 @@ class HomeView: BaseViewController, Navigatable {
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        actionBind()
     }
     // MARK: - Temp
     // MARK: - Init
@@ -31,10 +32,17 @@ class HomeView: BaseViewController, Navigatable {
     var viewModel: HomeViewModel!
     var calendarViewModel = CalendarViewModel()
     var disposedBag = DisposeBag()
-    var year: Int = 0
-    var month: Int = 0
-    var day: Int = 0
+    var headYear: Int = 0
+    var headMonth: Int = 0
+    var headDay: Int = 0
     var selectDayIndex: Int?
+    var calendarYear: Int?
+    var calendarMonth: Int?
+    var calendarDay: Int?
+    var monthCollectionViewHeight: Int?
+    var dateCheck: Int = 0
+    var calendarSelectIndex: Int?
+    var selectMonth: Int = 0
     // MARK: - UI Properties
     var listBtn = UIButton()
     var headTitleLbl = UILabel()
@@ -42,8 +50,43 @@ class HomeView: BaseViewController, Navigatable {
     var diaryBtn = UIButton()
     var headFrame = UIView()
     
-    var weekCalendarCollectionView: UICollectionView = {
+    var calendarFrame = UIView()
+    var calendarView = UIView().then({
+        $0.layer.cornerRadius = 8
+        $0.layer.masksToBounds = true
+        $0.backgroundColor = UIColor(asset: Asset.Color.monoWhite)
+    })
+    var calendarCloseBtn = UIButton()
+    var calendarDateLbl = UILabel().then({
+        $0.textColor = UIColor(asset: Asset.Color.monoDark010)
+        $0.font = FontFamily.Pretendard.bold.font(size: 16)
+    })
+    var btnPrev = UIButton().then({
+        $0.setImage(UIImage(asset: Asset.Icon.chevronLeft), for: .normal)
+    })
+    var btnNext = UIButton().then({
+        $0.setImage(UIImage(asset: Asset.Icon.chevronRight), for: .normal)
+    })
+    
+    var weekDayCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: weekDayCellLayout())
+        collectionView.register(CalendarWeekDayCell.self, forCellWithReuseIdentifier: "CalendarWeekDayCell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor(asset: Asset.Color.monoWhite)
+        return collectionView
+    }()
+    
+    var dayCollectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: dayDayCellLayout())
+        collectionView.register(HomeCalendarCell.self, forCellWithReuseIdentifier: "HomeCalendarCell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.isScrollEnabled = false
+        collectionView.backgroundColor = UIColor(asset: Asset.Color.monoWhite)
+        return collectionView
+    }()
+    
+    var weekCalendarCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: weekCalendarDayCellLayout())
         collectionView.register(WeekDayCalendarCell.self, forCellWithReuseIdentifier: "WeekDayCalendarCell")
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.isScrollEnabled = false
@@ -51,7 +94,7 @@ class HomeView: BaseViewController, Navigatable {
         return collectionView
     }()
     
-    static func dayDayCellLayout() -> UICollectionViewFlowLayout {
+    static func weekCalendarDayCellLayout() -> UICollectionViewFlowLayout {
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         layout.scrollDirection = .vertical
@@ -60,16 +103,45 @@ class HomeView: BaseViewController, Navigatable {
         layout.itemSize = CGSize(width: (UIScreen.main.bounds.width - 21 - (10 * 6)) / 7, height: 56)
         return layout
     }
+    
+    static func weekDayCellLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 32.83
+        layout.minimumLineSpacing = 0
+        layout.itemSize = CGSize(width: (UIScreen.main.bounds.width - 52 - (32.83 * 6)) / 7, height: 18)
+        return layout
+    }
+    
+    static func dayDayCellLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 23
+        layout.minimumLineSpacing = 10
+        layout.itemSize = CGSize(width: (UIScreen.main.bounds.width - 42 - (23 * 6)) / 7, height: 28)
+        return layout
+    }
+    
     // MARK: - InitUI
     override func initUI() {
         view.backgroundColor = UIColor(asset: Asset.Color.monoWhite)
         headTitleLbl.text = calendarViewModel.todayFormatteryyMMdd()
         headFrame = CommonView.homeHeadFrame(listIconBtn: listBtn, headTitle: headTitleLbl, dropDownButton: headDropDownBtn, diaryBtn: diaryBtn)
+        
+        calendarYear = calendarViewModel.getYear()
+        calendarMonth = calendarViewModel.getMonth()
+        calendarDay = calendarViewModel.getToday()
+        calendarDateLbl.text = "\(calendarYear ?? 0)년 \(calendarMonth ?? 0)월"
+        monthCollectionViewHeight = 7
+        calendarFrame = homeCalendarView()
     }
     // MARK: - LayoutSetting
     override func layoutSetting() {
         view.addSubview(headFrame)
         view.addSubview(weekCalendarCollectionView)
+        view.addSubview(calendarFrame)
         headFrame.snp.makeConstraints({
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             $0.leading.equalTo(view.snp.leading)
@@ -82,22 +154,40 @@ class HomeView: BaseViewController, Navigatable {
             $0.trailing.equalTo(view.snp.trailing).offset(-10.5)
             $0.height.equalTo(68)
         })
+        calendarFrame.snp.makeConstraints({
+            $0.top.equalTo(headFrame.snp.bottom)
+            $0.leading.equalTo(view.snp.leading)
+            $0.trailing.equalTo(view.snp.trailing)
+            $0.bottom.equalTo(view.snp.bottom)
+        })
     }
     // MARK: - Bind _ Output
     override func bind() {
+        calendarViewModel.monthObservable.subscribe( onNext: { [weak self] in
+            self?.calendarMonth = $0
+            self?.calendarDateLbl.text = "\(self?.calendarYear ?? 0)년 \($0)월"
+        }).disposed(by: disposedBag)
+        calendarViewModel.yearObservable.subscribe( onNext: { [weak self] in
+            self?.calendarYear = $0
+            self?.calendarDateLbl.text = "\($0)년 \(self?.calendarMonth ?? 0)월"
+        }).disposed(by: disposedBag)
+        
         calendarViewModel.weekDayListObservable // 주달력 데이터 [String]
             .bind(to: self.weekCalendarCollectionView.rx.items(cellIdentifier: "WeekDayCalendarCell", cellType: WeekDayCalendarCell.self)) { index, item, cell in
                 cell.weekDayLbl.text = self.calendarViewModel.getWeekDayList()[index]
                 cell.numberLbl.text = "\(item)"
-                if "\(item)" == self.calendarViewModel.todaydd() {
+                if ("\(item)" == self.calendarViewModel.todaydd()) || "0\(item)" == self.calendarViewModel.todaydd() {
                     cell.isToday = true
+                    cell.numberLbl.textColor = UIColor(asset: Asset.Color.monoWhite)
                 } else {
                     cell.isToday = false
+                    cell.numberLbl.textColor = UIColor(asset: Asset.Color.monoDark010)
                 }
             }.disposed(by: disposedBag)
-        calendarViewModel.monthObservable.subscribe { self.month = $0 }.disposed(by: disposedBag)
-        calendarViewModel.yearObservable.subscribe { self.year = $0 }.disposed(by: disposedBag)
-        calendarViewModel.weekDayList(currentMonth: month, currentYear: year)
+        calendarViewModel.monthObservable.subscribe { self.headMonth = $0 }.disposed(by: disposedBag)
+        calendarViewModel.yearObservable.subscribe { self.headYear = $0 }.disposed(by: disposedBag)
+        monthCollectionViewHeight = calendarViewModel.rowCount(currentMonth: self.headMonth, currentYear: self.headYear)
+        calendarViewModel.weekDayList(currentMonth: headMonth, currentYear: headYear)
         
         weekCalendarCollectionView.rx.itemSelected.subscribe(onNext: { indexPath in
             for i in 0..<7 {
@@ -110,12 +200,182 @@ class HomeView: BaseViewController, Navigatable {
             }
         }).disposed(by: disposedBag)
         
-        headDropDownBtn.rx.tap.bind(onNext: { _ in
-            print("dropDownClick")
+        calendarViewModel.weekDay // 월 ~ 일
+            .bind(to: self.weekDayCollectionView.rx.items(cellIdentifier: "CalendarWeekDayCell", cellType: CalendarWeekDayCell.self)) { index, item, cell in
+                if (index % 7) == 6 {
+                    cell.dayWeekLabel.textColor = UIColor(asset: Asset.Color.error)
+                } else {
+                    cell.dayWeekLabel.textColor = UIColor(asset: Asset.Color.monoDark010)
+                }
+                cell.dayWeekLabel.text = item
+            }.disposed(by: disposedBag)
+        
+        calendarViewModel.collectionViewHeight.subscribe { self.dayCollectionViewRemakeConstraint(count: $0) }.disposed(by: disposedBag)
+        
+        calendarViewModel.daylist // 월 달력 데이터 [String]
+            .bind(to: self.dayCollectionView.rx.items(cellIdentifier: "HomeCalendarCell", cellType: HomeCalendarCell.self)) { index, item, cell in
+                if item == "emptyCell" {
+                    cell.number.isHidden = false
+                    cell.number.text = ""
+                    cell.isUserInteractionEnabled = false
+                } else {
+                    cell.number.isHidden = false
+                    cell.number.text = item
+                    cell.isUserInteractionEnabled = true
+                }
+                if self.dateCheck == 0 && (("\(item)" == self.calendarViewModel.todaydd()) || "0\(item)" == self.calendarViewModel.todaydd()) {
+                    cell.isToday = true
+                    if (index % 7) == 6 {
+                        cell.number.textColor = UIColor(asset: Asset.Color.monoWhite)
+                    } else {
+                        cell.number.textColor = UIColor(asset: Asset.Color.monoDark010)
+                    }
+                } else {
+                    cell.isToday = false
+                    if (index % 7) == 6 {
+                        cell.number.textColor = UIColor(asset: Asset.Color.error)
+                    } else {
+                        cell.number.textColor = UIColor(asset: Asset.Color.monoDark010)
+                    }
+                }
+                
+                if self.calendarSelectIndex != nil {
+                    if self.dateCheck == self.selectMonth && self.calendarSelectIndex == index {
+                        cell.selectDayRoundFrame.isHidden = false
+                    } else {
+                        cell.selectDayRoundFrame.isHidden = true
+                    }
+                }
+                
+            }.disposed(by: disposedBag)
+        
+        dayCollectionView.rx.itemSelected.subscribe(onNext: { indexPath in
+            self.calendarSelectIndex = indexPath.row
+            for i in 0..<37 {
+                let cell = self.dayCollectionView.cellForItem(at: [0, i]) as? HomeCalendarCell
+                if i == indexPath.row {
+                    cell?.selectDayRoundFrame.isHidden = false
+                    print("cell.number.text : \(cell?.number.text ?? "")")
+                } else {
+                    cell?.selectDayRoundFrame.isHidden = true
+                }
+            }
+            self.selectMonth = self.dateCheck
         }).disposed(by: disposedBag)
+        
+        headDropDownBtn.rx.tap.bind(onNext: { _ in
+            self.calendarFrame.isHidden = false
+        }).disposed(by: disposedBag)
+        
+        calendarCloseBtn.rx.tap.bind(onNext: { _ in
+            self.calendarFrame.isHidden = true
+        }).disposed(by: disposedBag)
+        
     }
     
     // MARK: - Action Bind _ Input
+    func actionBind() {
+        
+        headDropDownBtn.rx.tap.bind(onNext: { _ in
+            self.calendarFrame.isHidden = false
+        }).disposed(by: disposedBag)
+        
+        calendarCloseBtn.rx.tap.bind(onNext: { _ in
+            self.calendarFrame.isHidden = true
+        }).disposed(by: disposedBag)
+        
+        self.btnPrev.rx.tap.bind {
+            self.dateCheck -= 1
+            self.calendarViewModel.getLastMonth(currentMonth: self.calendarMonth!, currentYear: self.calendarYear!)
+        }.disposed(by: disposedBag)
+        self.btnNext.rx.tap.bind {
+            self.dateCheck += 1
+            self.calendarViewModel.getNextMonth(currentMonth: self.calendarMonth!, currentYear: self.calendarYear!)
+        }.disposed(by: disposedBag)
+    }
     // MARK: - Other
+    func cellSelectInit() {
+        print("select init")
+        for i in 0..<37 {
+            let cell = self.dayCollectionView.cellForItem(at: [0, i]) as? HomeCalendarCell
+            cell?.selectDayRoundFrame.isHidden = true
+        }
+    }
+    
+}
+
+extension HomeView {
+    // MARK: - HomeCalendarView
+    /**
+     # homeCalendarView
+     - Authors: tavi
+     - returns: UIView
+     - Note: 홈의 월캘린더를 UIView로 리턴하는 함수
+     */
+    func homeCalendarView() -> UIView {
+        let view = UIView().then({
+            $0.backgroundColor = UIColor(asset: Asset.Color.monoDark010)?.withAlphaComponent(0.5)
+            $0.isHidden = true
+        })
+        
+        view.addSubview(calendarView)
+        calendarView.addSubview(btnPrev)
+        calendarView.addSubview(btnNext)
+        calendarView.addSubview(calendarDateLbl)
+        calendarView.addSubview(weekDayCollectionView)
+        calendarView.addSubview(dayCollectionView)
+        view.addSubview(calendarCloseBtn)
+        
+        calendarView.snp.makeConstraints({
+            $0.top.equalTo(view.snp.top).offset(-20)
+            $0.leading.equalTo(view.snp.leading)
+            $0.trailing.equalTo(view.snp.trailing)
+            $0.bottom.equalTo(view.snp.top).offset(340)
+        })
+        calendarDateLbl.snp.makeConstraints({
+            $0.centerX.equalTo(view.snp.centerX)
+            $0.top.equalTo(view.snp.top).offset(36)
+        })
+        btnPrev.snp.makeConstraints({
+            $0.centerY.equalTo(calendarDateLbl.snp.centerY)
+            $0.trailing.equalTo(calendarDateLbl.snp.leading).offset(-29)
+            $0.width.height.equalTo(24)
+        })
+        btnNext.snp.makeConstraints({
+            $0.centerY.equalTo(calendarDateLbl.snp.centerY)
+            $0.leading.equalTo(calendarDateLbl.snp.trailing).offset(29)
+            $0.width.height.equalTo(24)
+        })
+        calendarCloseBtn.snp.makeConstraints({
+            $0.top.equalTo(calendarView.snp.bottom)
+            $0.leading.equalTo(view.snp.leading)
+            $0.trailing.equalTo(view.snp.trailing)
+            $0.bottom.equalTo(view.snp.bottom)
+        })
+        weekDayCollectionView.snp.makeConstraints({
+            $0.top.equalTo(calendarDateLbl.snp.bottom).offset(20)
+            $0.leading.equalTo(view.snp.leading).offset(26)
+            $0.trailing.equalTo(view.snp.trailing).offset(-26)
+            $0.height.equalTo(12)
+        })
+        dayCollectionView.snp.makeConstraints({
+            $0.top.equalTo(weekDayCollectionView.snp.bottom).offset(16)
+            $0.leading.equalTo(calendarView.snp.leading).offset(20)
+            $0.trailing.equalTo(calendarView.snp.trailing).offset(-20)
+            $0.height.equalTo(38 * monthCollectionViewHeight!)
+        })
+        
+        return view
+    }
+    
+    // MARK: - Other
+    func dayCollectionViewRemakeConstraint(count: Int) {
+        dayCollectionView.snp.remakeConstraints({
+            $0.top.equalTo(weekDayCollectionView.snp.bottom).offset(16)
+            $0.leading.equalTo(calendarView.snp.leading).offset(20)
+            $0.trailing.equalTo(calendarView.snp.trailing).offset(-20)
+            $0.height.equalTo(38 * count)
+        })
+    }
     
 }
