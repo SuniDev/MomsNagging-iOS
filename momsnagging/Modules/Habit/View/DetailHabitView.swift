35d8da3938bf6ef02958efdbcffbe4068b0c3caa
suni <sun-918@naver.com>
2022-05-09 13:04:10 +0900

    faet : 습관 상세 UI 작업 진행 중

--- momsnagging/Modules/Habit/View/DetailHabitView.swift
+++ momsnagging/Modules/Habit/View/DetailHabitView.swift
@@ -32,7 +32,38 @@ class DetailHabitView: BaseViewController, Navigatable {
     lazy var viewContents = UIView()
     lazy var viewNameTitle = UIView()
     lazy var tfName = UITextField()
-    lazy var lblTime = UILabel()
+    lazy var divider1 = UIView()
+    lazy var viewTimeTitle = UIView()
+    lazy var viewTime = UIView()
+    lazy var lblTime = UILabel().then({
+        $0.text = "아직 정해지지 않았단다"
+        $0.textColor = Asset.Color.monoDark030.color
+        $0.font = FontFamily.Pretendard.regular.font(size: 14)
+    })
+    lazy var divider2 = UIView()
+    lazy var viewCycleTitle = UIView()
+    lazy var viewCycleType = UIView().then({
+        $0.addBorder(color: Asset.Color.monoLight020.color, width: 1)
+        $0.layer.cornerRadius = 8
+    })
+    lazy var btnCycleWeek = CommonButton().then({
+        $0.layer.cornerRadius = 5
+        $0.normalBackgroundColor = Asset.Color.monoWhite.color
+        $0.highlightedBackgroundColor = .clear
+        $0.selectedBackgroundColor = Asset.Color.priLight010.color
+        $0.setTitle("요일", for: .normal)
+        $0.setTitleColor(Asset.Color.monoDark030.color, for: .normal)
+        $0.setTitleColor(Asset.Color.monoWhite.color, for: .selected)
+    })
+    lazy var btnCycleNumber = CommonButton().then({
+        $0.layer.cornerRadius = 5
+        $0.normalBackgroundColor = Asset.Color.monoWhite.color
+        $0.highlightedBackgroundColor = .clear
+        $0.selectedBackgroundColor = Asset.Color.priLight010.color
+        $0.setTitle("N회", for: .normal)
+        $0.setTitleColor(Asset.Color.monoDark030.color, for: .normal)
+        $0.setTitleColor(Asset.Color.monoWhite.color, for: .selected)
+    })
     
     // MARK: - init
     init(viewModel: DetailHabitViewModel, navigator: Navigator) {
@@ -60,8 +91,7 @@ class DetailHabitView: BaseViewController, Navigatable {
         viewHeader = CommonView.detailHeadFrame(btnBack: btnBack, lblTitle: lblTitle, btnDone: btnDone)
         scrollView = CommonView.scrollView(viewContents: viewContents, bounces: true)
         viewNameTitle = CommonView.requiredTitleFrame("습관 이름")
-        tfName = CommonView.textField()
-        
+         
     }
     
     // MARK: - layoutSetting
