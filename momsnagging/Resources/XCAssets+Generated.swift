ccc60b0779778106e54bab4d26b60391512f1b17
jcp1452@gmail.com <connor.jeon@babyfriends.us>
2022-05-09 21:56:19 +0900

    [ Feat ] _ 충돌내용 해결

--- momsnagging/Resources/XCAssets+Generated.swift
+++ momsnagging/Resources/XCAssets+Generated.swift
@@ -115,12 +115,12 @@ internal enum Asset {
     internal static let reportCard = ImageAsset(name: "reportCard")
     internal static let sliders = ImageAsset(name: "sliders")
     internal static let straightLeft = ImageAsset(name: "straight_left")
-    internal static let todoAddFloating = ImageAsset(name: "todoAddFloating")
-    internal static let todoNonSelect = ImageAsset(name: "todoNonSelect")
-    internal static let todoSelect = ImageAsset(name: "todoSelect")
     internal static let x = ImageAsset(name: "x")
     internal static let xFloating = ImageAsset(name: "xFloating")
     internal static let xCircle = ImageAsset(name: "x_circle")
+    internal static let todoAddFloating = ImageAsset(name: "todoAddFloating")
+    internal static let todoNonSelect = ImageAsset(name: "todoNonSelect")
+    internal static let todoSelect = ImageAsset(name: "todoSelect")
   }
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
