90cd85cbf3dc955bc9b4d4cec42415ea1c6d8565
SuniDev <sun-918@naver.com>
2024-01-04 13:18:23 +0900

    [init] 프로제트 초기 세팅
    - Podfile 세팅
    - 폴더 구조 세팅

---
+++ momsnagging_ios/momsnagging_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  momsnagging_ios
+//
+//  Created by suni on 2024/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ momsnagging_ios/momsnagging_iosUITests/momsnagging_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  momsnagging_iosUITestsLaunchTests.swift
+//  momsnagging_iosUITests
+//
+//  Created by suni on 2024/01/03.
+//
+
+import XCTest
+
+final class momsnagging_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
