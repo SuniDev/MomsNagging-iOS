c5e2177fb8aea2141d986856a24d32159c6e0c73
SuniDev <sun-918@naver.com>
2024-09-19 13:46:22 +0900

    fix : 닉네임 세팅 View init 오류 수정

--- momsnagging/Sources/Scenes/NicknameSetting/NicknameSettingViewController.swift
+++ momsnagging/Sources/Scenes/NicknameSetting/NicknameSettingViewController.swift
@@ -26,6 +26,11 @@ class NicknameSettingViewController: BaseViewController {
         self.reactor = reactor
     }
     
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
