a6535f471c14014a85c4319b65faeeeab5633e7f
SuniDev <sun-918@naver.com>
2024-09-20 10:28:43 +0900

    feat : 공통 HintLabel, TextField 추가

---
+++ momsnagging/Sources/Common/Extensions/UITextField+Extension.swift
@@ -0,0 +1,49 @@
+//
+//  UITextField+Extension.swift
+//  momsnagging
+//
+//  Created by suni on 9/29/24.
+//
+
+import UIKit
+
+extension UITextField {
+    /**
+     # addLeftPadding
+     - parameters:
+        - width : 패딩 간격
+     - Authors: suni
+     - Note: 왼쪽 간격 추가 공통 함수.
+     */
+    func addLeftPadding(width: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
+    
+    /**
+     # addRightAndLeftPadding
+     - parameters:
+        - width : 패딩 간격
+     - Authors: suni
+     - Note: 양쪽 간격 추가 공통 함수.
+     */
+    func addRightAndLeftPadding(width: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+        self.rightView = paddingView
+        self.rightViewMode = ViewMode.always
+    }
+    
+    /**
+     # addPlaceHolder
+     - parameters:
+        - text : 플레이스홀더 택스트
+     - Authors: suni
+     - Note: 플레이스 홀더 추가 공통 함수.
+     */
+    func addPlaceHolder(_ text: String) {
+        self.attributedPlaceholder = NSAttributedString(string: text, attributes: [NSAttributedString.Key.foregroundColor: Asset.Color.monoDark030.color])
+    }
+}
---
+++ momsnagging/Sources/Common/Utils/AppTextField.swift
@@ -0,0 +1,67 @@
+//
+//  AppTextField.swift
+//  momsnagging
+//
+//  Created by suni on 9/29/24.
+//
+import UIKit
+
+/**
+ # (C) AppTextField
+ - Authors: suni
+ - Note: 공통 텍스트 필드 기본 기능 정의
+ */
+class AppTextField: UITextField {
+        
+    override var placeholder: String? {
+        didSet {
+            if let placeholder = placeholder {
+                self.addPlaceHolder(placeholder)
+            }
+        }
+    }
+    
+    @IBInspectable var normalBorderColor: UIColor = Asset.Color.monoLight010.color {
+        didSet {
+            normal()
+        }
+    }
+    @IBInspectable var errorBorderColor: UIColor = Asset.Color.error.color
+    @IBInspectable var successBorderColor: UIColor = Asset.Color.monoLight010.color
+    @IBInspectable var editBorderColor: UIColor = Asset.Color.monoDark030.color
+
+    func normal() {
+        self.addBorder(color: normalBorderColor, width: 1)
+    }
+    
+    func error() {
+        self.addBorder(color: errorBorderColor, width: 1)
+    }
+    
+    func success() {
+        self.addBorder(color: successBorderColor, width: 1)
+    }
+    
+    func edit() {
+        self.addBorder(color: editBorderColor, width: 1)
+    }
+    
+    // MARK: init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initview()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initview()
+    }
+    
+    private func initview() {
+        self.textColor = Asset.Color.monoDark010.color
+        self.layer.cornerRadius = 4
+        self.addLeftPadding(width: 8)
+        self.font = FontFamily.Pretendard.regular.font(size: 14)
+        self.normal()
+    }
+}
