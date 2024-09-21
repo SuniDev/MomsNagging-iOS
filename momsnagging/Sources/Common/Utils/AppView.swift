122a3bfa9f2eb63835edc7fefd12ac21ab72183a
SuniDev <sun-918@naver.com>
2024-09-21 14:17:52 +0900

    feat : 공통 ScrollView, hintTextFieldView 추가

---
+++ momsnagging/Sources/Common/Utils/AppView.swift
@@ -0,0 +1,74 @@
+//
+//  AppView.swift
+//  momsnagging
+//
+//  Created by suni on 9/29/24.
+//
+
+import UIKit
+import SnapKit
+
+class AppView {
+    
+    
+    /**
+     # scrollView
+     - parameters:
+        - viewContents : 컨텐츠 View
+        - bounces : 스크롤뷰 바운스 여부
+     - Authors: suni
+     - Returns: UIScrollView
+     - Note: 공통 스크롤 뷰 함수
+     */
+    static func scrollView(viewContents: UIView, bounces: Bool) -> UIScrollView {
+        lazy var scrollView = UIScrollView().then({
+            $0.bounces = bounces
+        })
+        
+        _ = viewContents.then({
+            $0.backgroundColor = .clear
+        })
+        
+        scrollView.addSubview(viewContents)
+        
+        viewContents.snp.makeConstraints({
+            $0.top.leading.trailing.bottom.equalToSuperview()
+            $0.width.equalTo(UIScreen.main.bounds.width)
+        })
+        
+        return scrollView
+    }
+    
+    /**
+     # hintTextFieldView
+     - parameters:
+        - tf : textField
+        - lblHint : 텍스트 힌트 Label
+     - Authors: suni
+     - Returns: UITextField
+     - Note: 텍스트 힌트가 있는 텍스트 필드 View
+     */
+    static func hintTextFieldView(tf: UITextField, lblHint: UILabel) -> UIView {
+        
+        lazy var viewHint = UIView().then({
+            $0.backgroundColor = .clear
+        })
+       
+        viewHint.addSubview(tf)
+        viewHint.addSubview(lblHint)
+        
+        tf.snp.makeConstraints({
+            $0.height.equalTo(48)
+            $0.top.leading.trailing.equalToSuperview()
+        })
+        
+        lblHint.snp.makeConstraints({
+            $0.top.equalTo(tf.snp.bottom).offset(5)
+            $0.leading.trailing.equalTo(tf)
+            $0.bottom.equalToSuperview()
+        })
+        
+        return viewHint
+    }
+    
+}
